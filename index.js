78f1593792aa8952c1d645b7fb7a6f64b77d5a82
ahmadid-xyz <ahmadcraft549@gmail.com>
2025-03-09 15:19:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -60,6 +60,23 @@ app.get("/api/openai", async (req, res) => {
   }
 });
 
+app.get("/api/islamai", async (req, res) => {
+  const { q } = req.query
+  if (!q) {
+    return res.status(400).json({ status: false, error: "Query is required" })
+  }
+  try {
+    const { islamai } = require('./scrape')
+    const response = await islamai(`${Enc(q)}`)
+    res.status(200).json({
+    status: true,
+    result: response.result
+    })
+  } catch (error) {
+    res.status(500).json({ status: false, error: error.message })
+  }
+});
+
 app.get('/api/lahelu', async (req, res) => {
  const { q } = req.query;
 
