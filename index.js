e7d5df1c27145d12bb4876c073250f0aa70c1632
ahmadid-xyz <ahmadcraft549@gmail.com>
2025-03-09 15:52:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -74,6 +74,20 @@ app.get("/api/openai", async (req, res) => {
   }
 });
 
+app.get('/api/bratv1', async (req, res) => {
+  const { q } = req.query
+  if (!q) {
+    return res.status(400).json({ status: false, error: "Query is required" })
+  }
+  try {
+    const response = await axios.get(`https://brat.caliphdev.com/api/brat?text=${Enc(q)}`, { responseType: 'arraybuffer' })
+    res.setHeader('Content-Type', 'image/png')
+    res.send(response.data)
+  } catch (error) {
+    res.status(500).json({ status: false, error: error.message })
+  }
+});
+
 app.get("/api/islamai", async (req, res) => {
   const { q } = req.query
   if (!q) {
