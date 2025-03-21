2212c7ba6f01580eeca7e81b1bb8677eb6a83064
ahmadid-xyz <ahmadcraft549@gmail.com>
2025-03-21 22:52:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -44,6 +44,23 @@ app.get('/stats', (req, res) => {
  res.json(stats);
 });
 
+app.get("/api/yts", async (req, res) => {
+  const { q } = req.query
+  if (!q) {
+    return res.status(400).json({ status: false, error: "Query is required" })
+  }
+  try {
+    const { ytsearch } = require('./search/functions')
+    const videos = await ytsearch(`${Dec(q)}`)
+    res.status(200).json({
+      status: true,
+      data: videos
+    })
+  } catch (error) {
+    res.status(500).json({ status: false, error: error.message })
+  }
+});
+
 app.get("/api/appstore", async (req, res) => {
   const { q } = req.query
   if (!q) {
