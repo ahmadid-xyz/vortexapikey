54f68a4a61bf86522db486b329615643f126aece
ahmadid-xyz <ahmadcraft549@gmail.com>
2025-03-22 08:19:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -44,6 +44,23 @@ app.get('/stats', (req, res) => {
  res.json(stats);
 });
 
+app.get("/api/islamai", async (req, res) => {
+  const { q } = req.query
+  if (!q) {
+    return res.status(400).json({ status: false, error: "Query is required" })
+  }
+  try {
+    const { islamai } = require('./scrape')
+    const response = await islamai(`${Enc(q)}`)
+    res.status(200).json({
+    status: true,
+    result: response.result
+    })
+  } catch (error) {
+    res.status(500).json({ status: false, error: error.message })
+  }
+});
+
 app.get("/api/bratv1", async (req, res) => {
   const { q } = req.query
   if (!q) {
